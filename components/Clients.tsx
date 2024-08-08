fc06744abeb973d000f4a3f6ac4a6f947c0dff5e
ali tarichi <tarichiali@gmail.com>
2024-08-08 18:38:38 +0300

    testimonials card styles fixed

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -10,17 +10,11 @@ const Clients = () => {
         <span className="text-purple"> satisfied clients</span>
       </h1>
       <div className="flex flex-col items-center">
-        <div
-          className="h-[50vh] md:h-[30rem] rounded-md
-        flex flex-col antialiased items-center relative
-        overflow-hidden"
-        >
-          <InfiniteMovingCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          />
-        </div>
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
       </div>
     </div>
   );
