acebd44f486fec6ae72f70761adfe5fa02201c91
ali tarichi <tarichiali@gmail.com>
2024-08-08 18:25:22 +0300

    tetimonal card added - infinte moving cards added

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { testimonials } from "@/data";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+      <div className="flex flex-col items-center">
+        <div
+          className="h-[50vh] md:h-[30rem] rounded-md
+        flex flex-col antialiased items-center relative
+        overflow-hidden"
+        >
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Clients;
