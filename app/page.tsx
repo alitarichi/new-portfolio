67324978cb6891f7c8882aa658b9d8a8bd06c549
ali tarichi <tarichiali@gmail.com>
2024-08-08 17:50:42 +0300

    clients section inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -16,6 +17,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
