f1931ac76536421c92258a55c8ad3c471a55cd29
ali tarichi <tarichiali@gmail.com>
2024-08-08 17:27:45 +0300

    navbar fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
@@ -11,13 +12,7 @@ export default function Home() {
       mx-auto sm:px-10 px-5"
     >
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            { name: "About", link: "/" },
-            { name: "Projects", link: "/" },
-            { name: "Testimonials", link: "/" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
