16423c73c14c923d7058d474c82641c9b0d412b6
ali tarichi <tarichiali@gmail.com>
2024-08-08 22:19:13 +0300

    experience inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -18,6 +19,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
---
+++ components/Experience.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="">
+      <h1 className="heading">
+        My Work
+        <span className="text-purple"> Experience</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-1"></div>
+    </div>
+  );
+};
+
+export default Experience;
