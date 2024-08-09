5e748ddec3fd721e113b3f9b5b0d2b71f455e0ba
ali tarichi <tarichiali@gmail.com>
2024-08-09 14:46:57 +0300

    footer section inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -22,6 +23,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
